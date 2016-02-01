2041eb7b8ff6fda58bf03b40fedfefb6afcf266a
vagrant <vagrant@precise32.(none)>
2016-02-01 21:10:02 +0000

    Hitting brick wall with Promise.all

--- app.js
+++ app.js
@@ -4,4 +4,21 @@ var url = process.argv[2] || "http://46.101.232.43/ ";
 
 var scraper = require("./lib/utility").scraper;
 
-scraper(url);
+//scraper(url);
+var loader = require("./lib/utility").promiseHtml;
+var linkExtractor = require("./lib/utility").linkExtractor;
+
+var p1 = loader(url)
+                    .then(function(html){
+                        //console.log(html);
+                        return linkExtractor(html);
+                    })
+                    .then(function(links){
+                      //console.log(links);
+                      return(links);
+                    });
+
+Promise.all(url.concat(p1[0])).then(function(urlString){
+                                  console.log(urlString);
+                                  return linkExtractor(urlString)
+                              });
