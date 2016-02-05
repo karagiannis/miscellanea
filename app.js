987bafb02436834942dff9492870eac7dedb103b
karagiannis <lasse.karagiannis@yaelev.se>
2016-02-05 16:11:17 +0100

    Response of booking POST received, commenting, and cleaning

--- app.js
+++ app.js
@@ -12,70 +12,91 @@ var loader = require("./lib/utility").promiseHtml;
 var linkExtractor = require("./lib/utility").linkExtractor;
 var okNotOkExtractor = require("./lib/utility").okNotOkExtractor;
 
+//Hard code input values for checking the film availibility, for test
+var day = "01";   //Friday = "01", Saturday = "02", Sunday = "03"
+var movie = "01";  //Söderkåkar, film number one in the list
 
 
-
+  //Load the html page of "http://46.101.232.43 "
   var mainPage = loader(url);
+
+  //Extract all links from mainpage at http://46.101.232.43
   var mainPageLinks = mainPage.then(function(html){  //Get the main html page from input url
                       //  console.log(html);
                         return linkExtractor(html);   //Extract all links
                     });
 
+//Grab the Calendar subUrl and append it to http://46.101.232.43, giving http://46.101.232.43/calendar/
   var calendarBaseUrl = mainPageLinks.then(function(links){
                       //console.log(url.trim().concat(links[0]));
                       return(url.trim().concat(links[0]));          // Send them along untouched
                     });
+
+//Grab the cinema subUrl and append it to http://46.101.232.43, giving http://46.101.232.43/cinema
   var cinemaUrl =  mainPageLinks.then(function(links){
                       //console.log(url.trim().concat(links[1]));
                       return(url.trim().concat(links[1]));
 
                     });
-
+//Grab the restaurant subUrl and append it to http://46.101.232.43, giving http://46.101.232.43/dinner/
+// THIS IS THE LOGIN URL
 var restaurantUrl = mainPageLinks.then(function(links){
                     //console.log("restaurant url", url.trim().concat(links[2]));
                     return(url.trim().concat(links[2]));
                   });
 
-  var calendarPage = calendarBaseUrl.then(function(BaseUrl){
+//Load the html of the calendar base url. This one contains the links to Paul's, Peter's and Mary's calendars
+var calendarPage = calendarBaseUrl.then(function(BaseUrl){
                         //console.log(BaseUrl);
                         return loader(BaseUrl);
                   });
-
-  var calendarLinks = calendarPage.then(function(html){
+//Grab the three subUrls to Paul, Peter and Mary
+var calendarLinks = calendarPage.then(function(html){
                       //console.log(html);
                       return linkExtractor(html);   //Extract all links
                   });
+
+  //Peter's calendar subUrl
   var PetersCalendarSubLink = calendarLinks.then(function(links){
                         //console.log("Peter sublink",links[1]);
                         return links[1].trim();
                   });
+
+  //Paul's calendar subUrl
   var PaulsCalendarSubLink = calendarLinks.then(function(links){
                       //  console.log("Paul sublink",links[0]);
                         return links[0].trim();
                   });
+
+  //Mary's calendar subUrl
   var MarysCalendarSubLink = calendarLinks.then(function(links){
                         //console.log("Mary sublink",links[2]);
                         return links[2].trim();
                   });
 
-  var PetersFullCalendarUrl = Promise.all([calendarBaseUrl,PetersCalendarSubLink])
-                                      .then(function(results){
-                                        //console.log("Peter ",results[0].concat("/").concat(results[1]));
-                                        return results[0].concat("/").concat(results[1]);
-                                      });
 
+// Full link to Pauls calendar. http://46.101.232.43/calendar/paul.html
 var PaulsFullCalendarUrl = Promise.all([calendarBaseUrl,PaulsCalendarSubLink])
                                     .then(function(results){
                                       //console.log("Paul ",results[0].concat("/").concat(results[1]));
                                       return results[0].concat("/").concat(results[1]);
                                     });
+
+//Full link to Peter's calendar. http://46.101.232.43/calendar/peter.html
+var PetersFullCalendarUrl = Promise.all([calendarBaseUrl,PetersCalendarSubLink])
+                                      .then(function(results){
+                                        //console.log("Peter ",results[0].concat("/").concat(results[1]));
+                                        return results[0].concat("/").concat(results[1]);
+                                      });
+//Full link to Mary's calendar. http://46.101.232.43/calendar/mary.html
 var MarysFullCalendarUrl = Promise.all([calendarBaseUrl,MarysCalendarSubLink])
                                     .then(function(results){
                                       //console.log("Mary", results[0].concat("/").concat(results[1]));
                                       return results[0].concat("/").concat(results[1]);
                                     });
 
-  var PetersCalendar = PetersFullCalendarUrl.then(function(calendarUrl){
+//Peter's calendar:  Peter [ 'ok', '--', 'ok' ] menaning friday OK, saturday Not OK, Sunday OK
+var PetersCalendar = PetersFullCalendarUrl.then(function(calendarUrl){
                                       return loader(calendarUrl);
                                     }).then(function(html){
                                       return okNotOkExtractor(html);
@@ -84,6 +105,7 @@ var MarysFullCalendarUrl = Promise.all([calendarBaseUrl,MarysCalendarSubLink])
                                       return calendar;
                                     });
 
+//Paul's calendar:  Paul  [ 'ok', 'OK', '--' ]
 var PaulsCalendar = PaulsFullCalendarUrl.then(function(calendarUrl){
                                     return loader(calendarUrl);
                                   }).then(function(html){
@@ -92,8 +114,8 @@ var PaulsCalendar = PaulsFullCalendarUrl.then(function(calendarUrl){
                                     //console.log("Paul ",calendar);
                                     return calendar;
                                   });
-
-  var MarysCalendar = MarysFullCalendarUrl.then(function(calendarUrl){
+// Mary's calendar:   Mary  [ 'ok', 'OK', '--' ]
+var MarysCalendar = MarysFullCalendarUrl.then(function(calendarUrl){
                                       return loader(calendarUrl);
                                     }).then(function(html){
                                       return okNotOkExtractor(html);
@@ -101,7 +123,11 @@ var PaulsCalendar = PaulsFullCalendarUrl.then(function(calendarUrl){
                                       //console.log("Mary ",calendar);
                                       return calendar;
                                     });
+
+//Used for converting integer value to string representing day, to be used with meetingDay routine
 var possibleDays = ["friday", "saturday", "sunday"];
+
+//Returns and array of possible meetingdays : arr [ true, false, false ]
 var meetingDay = Promise.all([PetersCalendar,PaulsCalendar,MarysCalendar])
                           .then(function(calendars){
                                   var arr = [];
@@ -118,82 +144,113 @@ var meetingDay = Promise.all([PetersCalendar,PaulsCalendar,MarysCalendar])
                                       //console.log("Possible meeting day",possibleDays[i] );
                                       console.log("");
                                   });
+                                  //console.log("arr", arr);
                                 return arr;
                           });
 
+//Loads the html of the cinema page
 var cinemaHTML = cinemaUrl.then(function(urlCinema){
                             //console.log("urlCinema", urlCinema);
                             return loader(urlCinema);
                       });
 
 var filmExtractor = require("./lib/utility").filmExtractor;
+
+// Extracts the films showed at cinema:   Cinema films [ 'Söderkåkar', 'Fabian Bom', 'Pensionat Paradiset', '' ]
 var cinemaFilms = cinemaHTML.then(function(html){
                             //console.log("cinema html", html)
                             return filmExtractor(html);
+                      }).then(function(data){
+                        //console.log("Cinema films", data);
+                        return data;
                       });
+
+// Old troubleshooting function. Prints out the cinema films array
 var cinemaFilmsPrintOut = cinemaFilms.then(function(arr){
                                 //console.log("List of films", arr);
                           });
-var filmAvailibility = require("./lib/utility").filmAvailibility;
+
+//Using npm package xmlhttprequest, on the server side!!!!
 var ajaxConfig = {contentType:"application/json",
                   url:"",
                   query:""};
-  var ajax = require("./lib/ajax");
+var ajax = require("./lib/ajax");
+
+// Returns an object with the avaibility of a certain film a certain day
+//[ { status: 0, day: '01', time: '16:00', movie: '01' }, { status: 1, day: '01', time: '18:00', movie: '01' }, { status: 0, day: '01', time: '21:00', movie: '01' } ]
 var availibleFilm = Promise.all([cinemaUrl, cinemaFilms]).then(function(results){
                             //console.log("results", results);
-                            ajaxConfig.url = results[0] + "/check?day=" + "01" + "&movie=" + "01";
+                            ajaxConfig.url = results[0] + "/check?day=" + day.trim() + "&movie=" + movie.trim();
                             //console.log("ajaxConfig.url", ajaxConfig.url);
                             return ajax.get(ajaxConfig);
-                    }).then(function(str){
-                      //console.log(str);
+                    }).then(function(JSONstr){
+                      var obj = JSON.parse(JSONstr);
+                      //console.log(obj);
+                      return obj;
                     });
+
+
 var loginDetailsExtractor = require("./lib/utility").loginDetailsExtractor;
+//Used to extract the key value pairs, to be combined in the form POST : login details [ 'username', 'password', 'submit', 'login', '' ]
+//Will be the request package be encoded to "username=zeke&password=coys&submit=login"
 var loginDetails = restaurantUrl.then(function(restauranturl){
                         return loader(restauranturl);
                   }).then(function(htmlPage){
                         return(loginDetailsExtractor(htmlPage));
                   }).then(function(data){
                     //console.log("login details", data);
+                    return data;
                   });
 
-  // var redirect = new Promise(function(resolve, reject){
-  //   var r = request.post({url:"http://46.101.232.43/dinner/login",
-  //               form: {username:'zeke',password:"coys",submit:"login"}},
-  //                     function(err,httpResponse,body)
-  //                     {
-  //                       if (err) {
-  //                          return reject(console.error('upload failed:', err));
-  //                        }
-  //                        //console.log(httpResponse);
-  //                        return resolve( body);
-  //                     });
-  //
-  // }).then(function(data){
-  //   var dataToArray = data.split(" ");
-  //   var subUrl = dataToArray[dataToArray.length -1];
-  //   //console.log(subUrl);
-  // });
 
 
  var redirecter = require("./lib/utility").redirecter;
 
-var redirectedBookingSubUrlAndCookie = restaurantUrl.then(function(loginUrl){
-                  return redirecter("zeke", "coys", "login", loginUrl + "/login");
-              }).then(function(redirectMessage){
-                //console.log(redirectMessage);
-                var redirectArray = redirectMessage;
-                var messageWithSubUrlIncluded = redirectArray[0].split(" ");
-                var subUrl = messageWithSubUrlIncluded[messageWithSubUrlIncluded.length -1];
-                //console.log("Suburl NOW ", subUrl);
-                var messageWithCookieIncludedTemp = redirectArray[1].split("[")[1].split("]")[0];
-                //console.log("messageWithCookieIncludedTemp ", messageWithCookieIncludedTemp);
-                var arr = [];
-                arr[0]=subUrl;
-                arr[1] = messageWithCookieIncludedTemp;//The cookie
-                return arr;
-              });
+// var redirectedBookingSubUrlAndCookie = restaurantUrl.then(function(loginUrl){
+//                   return redirecter("zeke", "coys", "login", loginUrl + "/login");
+//               }).then(function(redirectMessage){
+//                 console.log("redirectMessage",redirectMessage);
+//                 var redirectArray = redirectMessage;
+//                 var messageWithSubUrlIncluded = redirectArray[0].split(" ");
+//                 var subUrl = messageWithSubUrlIncluded[messageWithSubUrlIncluded.length -1];
+//                 //console.log("Suburl NOW ", subUrl);
+//                 var messageWithCookieIncludedTemp = redirectArray[1].split("[")[1].split("]")[0];
+//                 //console.log("messageWithCookieIncludedTemp ", messageWithCookieIncludedTemp);
+//                 var arr = [];
+//                 arr[0]=subUrl;
+//                 arr[1] = messageWithCookieIncludedTemp;//The cookie
+//                 return arr;
+//               });
+
+
+
+//Does the post of username and password and recieves redirect url + cookie :
+// arr  [ 'login/booking"','"node_session_cookie=s%3AYoBMFVwmIi6kv6Xflm9lDWBwwy0qxlw6.wIScZN0zTaxoh6v2Jot3N02cNkv69I8zP7Dv4JMp8%2Fs; Path=/; HttpOnly"' ]
+  var redirectedBookingSubUrlAndCookieEx = Promise.all([username,password,loginDetails,restaurantUrl]).then(function(results){
+                    var username = results[0]; //zeke
+                    var password = results[1]; //coys
+                    var valueForSubmit = results[2][3]; // "login"
+                    var urlForPOSTingLoginDetails = results[3] + "/login"; //http://46.101.232.43/dinner/login
+                    return redirecter(username, password, valueForSubmit, urlForPOSTingLoginDetails);
+                }).then(function(redirectMessage){
+                  //console.log("redirectMessage",redirectMessage);
+                  var redirectArray = redirectMessage;
+                  var messageWithSubUrlIncluded = redirectArray[0].split(" ");
+                  var subUrl = messageWithSubUrlIncluded[messageWithSubUrlIncluded.length -1];
+                  //console.log("Suburl NOW ", subUrl);
+                  var messageWithCookieIncludedTemp = redirectArray[1].split("[")[1].split("]")[0];
+                  //console.log("messageWithCookieIncludedTemp ", messageWithCookieIncludedTemp);
+                  var arr = [];
+                  arr[0]=subUrl;
+                  arr[1] = messageWithCookieIncludedTemp;//The cookie
+                  console.log("arr ", arr);
+                  return arr;
+                });
+
 var bookingsPageLoader = require("./lib/utility").bookingsPageLoader;
-var restaurantBookingHTML = Promise.all([restaurantUrl,redirectedBookingSubUrlAndCookie])
+
+//Loads the html of the restaurant bookings page
+var restaurantBookingHTML = Promise.all([restaurantUrl,redirectedBookingSubUrlAndCookieEx])
                                        .then(function(results){
                                          //console.log("results", results);
                                          var fullBookingUrlString = results[0]+"/"+results[1][0];
@@ -216,19 +273,55 @@ var restaurantBookingHTML = Promise.all([restaurantUrl,redirectedBookingSubUrlAn
 
 var availibleTables = require("./lib/utility").availibleTables;
 
+//Extracts the availbe days and times for booking a table, as well as a special token csrf_token to be used when posting
+//availible Tables [ 'group1','fre1416','group1','fre1618','group1','fre1820','group1','lor1820',
+                   //'group1','lor2022','group1','son1416','group1','son1618','group1','son1820',
+                   //'group1','son2022','csrf_token','Jishgeny6753ydiayYHSjay0918','' ]
 var availibleTablesList = restaurantBookingHTML.then(function(html){
                                           //console.log(html);
                                                     return availibleTables(html);
                                               }).then(function(data){
-                                                    console.log(data);
+                                                    //console.log("availible Tables", data);
                                                     return data;
                                                   });
 
-var bookATable = rquire("./lib/utility").bookATable;
-//bookATable(url,cookie, group,dayTimeString,csrftoken)
-var dayTimeString = son1618;
 
-var bookingATableNow = Promise.all([redirectedBookingSubUrlAndCookie,availibleTablesList,dayTimeString])
+
+var bookATable = require("./lib/utility").bookATable;
+
+//Hard coded string for table booking for debug purposes. To be set dynamically after film is decided and availible restaurant table presentation
+var dayTimeStringTableBooking = "lor1820";
+
+//Books a table and receives a response html page from the server
+var bookingATableNow = Promise.all([restaurantUrl,redirectedBookingSubUrlAndCookieEx,availibleTablesList,dayTimeStringTableBooking])
                               .then(function(results){
-                                
+
+                                var fullBookingUrlString = results[0]+"/"+results[1][0];
+                                fullBookingUrlString = fullBookingUrlString.trim().replace(/['"]+/g, '');
+                                var CookieString = results[1][1];
+                                CookieString = CookieString.replace(/['"]+/g, '');
+                            //    console.log("fullBookingUrlString", fullBookingUrlString);
+                            //    console.log("CookieString", CookieString);
+                                var avalibeTables = results[2];
+                                var lengthAvailibleTables =  avalibeTables.length;
+                              //  console.log("lengthAvailibleTables ", lengthAvailibleTables);
+                                var csrftoken = avalibeTables[lengthAvailibleTables -2];
+                              //  console.log("avalibeTables", avalibeTables);
+                              //  console.log("csrftoken", csrftoken);
+                                var dayTimeStr = results[3];
+                                //console.log("dayTimeStr",dayTimeStr);
+                                return bookATable(fullBookingUrlString,CookieString, "group1",dayTimeStr,csrftoken);
+                              }).then(function(html){
+                                //console.log("html", html);
+                                return html;
                               });
+
+var bookingsResponser = require("./lib/utility").bookingsResponser;
+
+// Extracts "Din bokning är gjord" from the previous received response, that is from the html page which the server responded to the booking POST.
+var bookingResponse = bookingATableNow.then(function(html){
+                              return bookingsResponser(html);
+                          }).then(function(greeting){
+                            console.log(greeting);
+                            return greeting;
+                          });
